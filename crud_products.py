4af7ce494064f9df2b027184d951ba5a6fdd6b4b
ValentinaLV <tina.lysenok@gmail.com>
2019-12-16 14:36:17 +0200

    updated crud_products.py + tests.py with postgres and w/o sqllite

--- crud_products.py
+++ crud_products.py
@@ -1,13 +1,11 @@
 """
 CRUD properties implementation
 """
-from datetime import date
-from typing import List
 
 
 def add_product(con, product_name: str, price: int, img: str) -> None:
     """
-    Add new task to db.
+    Add new product to db.
     :param con: str
     :param product_name: str
     :param price: int
@@ -15,23 +13,64 @@ def add_product(con, product_name: str, price: int, img: str) -> None:
     :return: None
     """
     with con.cursor() as cursor:
-            cursor.execute("""INSERT INTO postgres.public.products(productname, price, image)
+        cursor.execute("""INSERT INTO postgres.public.products(productname, price, image)
                             VALUES ('{0}', '{1}', '{2}')""".format(product_name, price, img))
     con.commit()
 
 
-def get_product(con, id: int) -> str:
+def get_product(con, product_id: int) -> str:
     """
     Get product from db using index parameter.
     :param con: str
-    :param id: int
+    :param product_id: int
     :return: str
     """
     cursor = con.cursor()
     cursor.execute("""SELECT productname FROM postgres.public.products
-                    WHERE productid = {0}""".format(id))
+                    WHERE productid = {0}""".format(product_id))
     result = cursor.fetchone()
     cursor.close()
     return result[0]
 
 
+def get_product_price(con, product_id: int) -> str:
+    """
+    Get product from db using index parameter.
+    :param con: str
+    :param product_id: int
+    :return: str
+    """
+    cursor = con.cursor()
+    cursor.execute("""SELECT price FROM postgres.public.products
+                    WHERE productid = {0}""".format(product_id))
+    result = cursor.fetchone()
+    cursor.close()
+    return result[0]
+
+
+def edit_product(con, product_id: int, new_price: int) -> None:
+    """
+    Update task in db.
+    :param con: str
+    :param new_price: int
+    :param product_id: int
+    :return: None
+    """
+    with con.cursor() as cursor:
+        cursor.execute("""UPDATE postgres.public.products
+                        SET price = '{0}'
+                        WHERE productid = '{1}'""".format(new_price, product_id))
+    con.commit()
+
+
+def delete_product(con, product_id: int) -> None:
+    """
+    Delete task in db.
+    :param con: str
+    :param product_id: int
+    :return: None
+    """
+    with con.cursor() as cursor:
+        cursor.execute("""DELETE FROM postgres.public.products 
+                        WHERE productid = {0}""".format(product_id))
+    con.commit()
--- tests.py
+++ tests.py
@@ -1,5 +1,3 @@
-import datetime
-
 import psycopg2
 import pytest
 
@@ -11,5 +9,21 @@ def test_create_product():
     con = psycopg2.connect(**DATABASE)
     crud_products.add_product(con, 'Bed', 3000,
                               'https://www.westelm.com/weimgs/ab/images/wcm/products/201940/0467/wright-bed-c.jpg')
-    assert crud_products.get_product(con, 13) == "Bed"
+    assert crud_products.get_product(con, 14) == "Bed"
 
+
+def test_update_product():
+    con = psycopg2.connect(**DATABASE)
+    crud_products.add_product(con, 'Bed', 3000,
+                              'https://www.westelm.com/weimgs/ab/images/wcm/products/201940/0467/wright-bed-c.jpg')
+    crud_products.edit_product(con, 16, 5100)
+    assert crud_products.get_product_price(con, 16) == 5100
+
+
+def test_delete_product():
+    con = psycopg2.connect(**DATABASE)
+    crud_products.add_product(con, 'Bed', 3000,
+                              'https://www.westelm.com/weimgs/ab/images/wcm/products/201940/0467/wright-bed-c.jpg')
+    crud_products.delete_product(con, 17)
+    with pytest.raises(TypeError):
+        crud_products.get_product(con, 17)

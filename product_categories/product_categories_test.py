4d823e54d6a5888478fd7657507330735f171947
Tomalia Andrii <logotip123@yahoo.com>
2019-12-16 15:40:28 +0200

    Create categorise logic

--- product_categories/product_categories_test.py
+++ product_categories/product_categories_test.py
@@ -4,22 +4,20 @@ import psycopg2
 import product_categories
 from config import DATABASE
 
+with psycopg2.connect(**DATABASE) as conn:
 
-def create_category():
-    conn = psycopg2.connect(**DATABASE)
-    product_categories.create_category(conn, "Soldering irons")
-    assert product_categories.read_category(conn, 1) == "Soldering irons"
+    def create_category():
+        product_categories.create_category(conn, "Soldering irons")
+        assert product_categories.read_category(conn, 1) == "Soldering irons"
 
-def update_category():
-    conn = psycopg2.connect(**DATABASE)
-    product_categories.create_category(conn, "furniturka")
-    product_categories.update_category(conn, 2, "Furniture")
-    assert product_categories.read_category(conn, 2) == "furniture"
+    def update_category():
+        product_categories.create_category(conn, "furniturka")
+        product_categories.update_category(conn, 2, "Furniture")
+        assert product_categories.read_category(conn, 2) == "Furniture"
 
-def delete_category():
-    conn = psycopg2.connect(**DATABASE)
-    product_categories.create_category(conn, "Wears")
-    product_categories.delete_category(conn, 3)
-    with pytest.raises(KeyError):
-        product_categories.read_category(conn, 3)
+    def delete_category():
+        product_categories.create_category(conn, "Wears")
+        product_categories.delete_category(conn, 3)
+        with pytest.raises(KeyError):
+            product_categories.read_category(conn, 3)
 

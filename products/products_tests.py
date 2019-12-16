6287dc985a09063c79aa344170c7718572700439
ValentinaLV <tina.lysenok@gmail.com>
2019-12-16 15:13:32 +0200

    added products folder

--- products/products_tests.py
+++ products/products_tests.py
@@ -1,29 +1,29 @@
 import psycopg2
 import pytest
 
-import crud_products
+from products import products
 from config import DATABASE
 
 
 def test_create_product():
     con = psycopg2.connect(**DATABASE)
-    crud_products.add_product(con, 'Bed', 3000,
+    products.add_product(con, 'Bed', 3000,
                               'https://www.westelm.com/weimgs/ab/images/wcm/products/201940/0467/wright-bed-c.jpg')
-    assert crud_products.get_product(con, 14) == "Bed"
+    assert products.get_product(con, 14) == "Bed"
 
 
 def test_update_product():
     con = psycopg2.connect(**DATABASE)
-    crud_products.add_product(con, 'Bed', 3000,
+    products.add_product(con, 'Bed', 3000,
                               'https://www.westelm.com/weimgs/ab/images/wcm/products/201940/0467/wright-bed-c.jpg')
-    crud_products.edit_product(con, 16, 5100)
-    assert crud_products.get_product_price(con, 16) == 5100
+    products.edit_product(con, 16, 5100)
+    assert products.get_product_price(con, 16) == 5100
 
 
 def test_delete_product():
     con = psycopg2.connect(**DATABASE)
-    crud_products.add_product(con, 'Bed', 3000,
+    products.add_product(con, 'Bed', 3000,
                               'https://www.westelm.com/weimgs/ab/images/wcm/products/201940/0467/wright-bed-c.jpg')
-    crud_products.delete_product(con, 17)
+    products.delete_product(con, 17)
     with pytest.raises(TypeError):
-        crud_products.get_product(con, 17)
+        products.get_product(con, 17)

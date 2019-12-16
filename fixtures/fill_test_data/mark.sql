caa03731a5189c87107e9ed08bda77199c815a0c
Senku1 <morhaliuk@gmail.com>
2019-12-16 16:32:27 +0200

    mark

---
+++ fixtures/fill_test_data/mark.sql
@@ -0,0 +1,3 @@
+INSERT INTO mark
+(id_user, id_product, mark_date, rating)
+VALUES (3, 7, '2019-10-21', 4)
---
+++ fixtures/init/mark.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE mark (
+    id_user INTEGER,
+    id_product INTEGER,
+    mark_date DATE,
+    rating INTEGER
+);

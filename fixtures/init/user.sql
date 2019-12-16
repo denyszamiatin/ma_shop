5ea9f767e98ba6825d0228f84b8861256842025c
KoretsValentyn <KoretsValentyn@gmail.com>
2019-12-17 00:23:44 +0200

    add users crud

--- fixtures/init/user.sql
+++ fixtures/init/user.sql
@@ -1,5 +1,5 @@
 CREATE TABLE user(
-    user_id serial primary key,
+    id SERIAL PRIMARY KEY,
     first_name VARCHAR(35) NOT NULL,
     second_name VARCHAR (50),
     email VARCHAR (50) NOT NULL unique,

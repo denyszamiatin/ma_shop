7dea3a2a02417c1191df3c1b2e3e9841a8f0319c
d1makov <38015733+d1makov@users.noreply.github.com>
2019-12-17 11:59:33 +0200

    Create ma_shop.py

---
+++ templates/ma_shop.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+def catalogue():
+    return render_template("catalogue.html")
+
+
+def product():
+    return render_template("product.html")
+
+
+def cart():
+    return render_template("cart.html")
+
+
+def news():
+    return render_template("news.html")
+
+
+def contacts():
+    return render_template("contacts.html")
+
+
+def login():
+    return render_template("login.html")
+
+
+def registration():
+    return render_template("registration.html")
+
+
+def product_comments():
+    return render_template("product_comments.html")

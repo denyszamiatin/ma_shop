e98417db078fa4a8fb76eb522b876c1702fe53df
d1makov <makovskyj.danylo@gmail.com>
2019-12-17 14:19:16 +0200

    final

--- cart/cart.py
+++ cart/cart.py
@@ -3,73 +3,58 @@ CRUD
 """
 
 
-def add_to(con, user_id: int, product_id: int) -> None:
+def add(conn, user_id: int, product_id: int) -> None:
     """
-    Add new prodact and 
+    Add new prodact and
     user id to cart.
-    :param con: str
-    :param id_user: int
-    :param id_product: int
+    :param conn: str
+    :param user_id: int
+    :param product_id: int
     :return: None
     """
-    with con.cursor() as cursor:
-        cursor.execute("""INSERT INTO postgres.public.cart(user_id, product_id)
-                            VALUES ('{}', '{}')""".format(user_id, product_id))
-        try:
-            cursor.fetchone()
-        except TypeError:
-            raise KeyError
-    con.commit()
+    with conn.cursor() as cursor:
+        cursor.execute(f"""INSERT INTO cart(user_id, product_id)
+                            VALUES ({user_id}, {product_id})""")
+    conn.commit()
 
 
-def get_all_from(con, user_id: int) -> list:
+def get_all(conn, user_id: int) -> list:
     """
     Get all products_id from db using index user_id.
-    :param con: str
-    :param product_id: int
+    :param conn: str
+    :param user_id: int
     :return: list
     """
-    with con.cursor() as cursor:
-        cursor.execute("""SELECT product_id FROM postgres.public.cart
-                    WHERE user_id = {}""".format(user_id))
-        try:
-            cursor.fetchone()
-        except TypeError:
-            raise KeyError
+    cursor = conn.cursor()
+    cursor.execute(f"""SELECT product_id FROM cart
+                    WHERE user_id = {user_id}""")
     result = cursor.fetchall()
+    cursor.close()
     return [''.join(i) for i in result]
 
 
-def get_one_from(con, user_id: int, prod) -> str:
-    """
-    Get product from db using index parameter.
-    :param con: str
-    :param product_id: int
-    :return: str
-    """
-    with con.cursor() as cursor:
-        cursor.execute("""SELECT product_id FROM postgres.public.cart
-                    WHERE id_user = {}""".format(user_id))
-        try:
-            cursor.fetchone()
-        except TypeError:
-            raise KeyError
-    result = cursor.fetchone()
-    return result[0]
-
-
-def delete_from(con, user_id, product_id,) -> None:
+def delete(conn, user_id, product_id) -> None:
     """
     Delete task in db.
-    :param con: str
+    :param conn: str
+    :param user_id: int
     :param product_id: int
     :return: None
     """
-    with con.cursor() as cursor:
-        cursor.execute("""DELETE FROM postgres.public.cart 
-                        WHERE (user_id, product_id) VALUES ({}, {})""".format(user_id, product_id))
-        try:
-            cursor.fetchone()
-        except TypeError:
-            raise KeyError
-    con.commit()
+    with conn.cursor() as cursor:
+        cursor.execute(f"""DELETE FROM cart 
+                        WHERE (user_id, product_id) VALUES ({user_id}, {product_id})""")
+    conn.commit()
+
+
+def delete_all(conn, user_id) -> None:
+    """
+    Delete task in db.
+    :param conn: str
+    :param user_id: int
+    :return: None
+    """
+    with conn.cursor() as cursor:
+        cursor.execute(f"""DELETE FROM cart 
+                        WHERE user_id = {user_id}""")
+    conn.commit()

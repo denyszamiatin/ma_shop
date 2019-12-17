94c2bc6789dac1bd742ff2013fdbe4ee10fef00d
d1makov <38015733+d1makov@users.noreply.github.com>
2019-12-17 12:08:15 +0200

    Update ma_shop.py

--- templates/ma_shop.py
+++ templates/ma_shop.py
@@ -10,38 +10,45 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/catalogue')
 def catalogue():
     return render_template("catalogue.html")
 
 
+@app.route('/product')
 def product():
     return render_template("product.html")
 
 
+@app.route('/cart')
 def cart():
     return render_template("cart.html")
 
 
+@app.route('/news')
 def news():
     return render_template("news.html")
 
 
+@app.route('/contacts')
 def contacts():
     return render_template("contacts.html")
 
 
+@app.route('/login')
 def login():
     return render_template("login.html")
 
 
+@app.route('/registration')
 def registration():
     return render_template("registration.html")
 
 
+@app.route('/product_comments')
 def product_comments():
     return render_template("product_comments.html")
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-

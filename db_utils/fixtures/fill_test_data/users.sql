102ede4c27c57d712c8c4cf7bfb7b875b94b2d25
Tomalia Andrii <logotip123@yahoo.com>
2019-12-18 16:27:41 +0200

    users fil_test_data fix

--- db_utils/fixtures/fill_test_data/users.sql
+++ db_utils/fixtures/fill_test_data/users.sql
@@ -1,3 +1,2 @@
-create EXTENSION pgcrypto;
-insert into users (first_name, second_name, email, password) VALUES ('Ivan','Ivanov','ivanov@gmail.com',crypt("gfhjkm123",gen_salt('bf')));
-insert into users (first_name, second_name, email, password) VALUES ('Roman','Petrenko','petrenko@gmail.com',crypt("admin123",gen_salt('bf')));
+insert into users (first_name, second_name, email, password) VALUES ('Ivan','Ivanov','ivanov@gmail.com', '1231231d23');
+insert into users (first_name, second_name, email, password) VALUES ('Roman','Petrenko','petrenko@gmail.com', '123123111ddd');

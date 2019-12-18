7fdd3549a9b3c1817e72a8df2099f924b9a882b5
Tomalia Andrii <logotip123@yahoo.com>
2019-12-18 17:25:14 +0200

    fix connect users

--- db_utils/fixtures/init/user.sql
+++ db_utils/fixtures/init/user.sql
@@ -1,7 +1,7 @@
 create table users (
 id serial primary key,
-first_name varchar(35) not null,
+first_name varchar(50) not null,
 second_name varchar (50),
-email varchar (50) not null unique,
-password varchar (100) not null
+email varchar (100) not null unique,
+password varchar (255) not null
 );

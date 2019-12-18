f527d71321dc22dd56f52c3e8de7a9cc45e96187
koreyo <KoretsValentyn@gmail.com>
2019-12-18 12:45:57 +0200

    change name in db

--- db_utils/fixtures/init/comments.sql
+++ db_utils/fixtures/init/comments.sql
@@ -1,7 +1,7 @@
 create table Comments (
-	comment_id serial primary key,
+	id serial primary key,
     product_id integer references product(id),
     user_id integer references user(id),
-    date TIMESTAMPTZ not null default NOW(),
+    comment_date TIMESTAMPTZ not null default NOW(),
     body TEXT
 );

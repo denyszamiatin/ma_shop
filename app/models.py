71d76be4a6465ed895ba5c56c4ef92acd2d9fa70
Senku1 <morhaliuk@gmail.com>
2019-12-26 16:15:54 +0200

    task 52 (class users)

--- app/models.py
+++ app/models.py
@@ -24,3 +24,14 @@ class Cart(db.Model):
     id_user = db.Column(db.Integer, db.ForeignKey('users.id'))
     id_product = db.Column(db.Integer, db.ForeignKey('products.id'))
     addition_date = db.Column(db.Date, default=datetime.today().date())
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    second_name = db.Column(db.String(50))
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f"<User id: {self.id}>"

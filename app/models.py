426eb04d35afd8a011b871449cb4e7e8e90f6c87
Tomalia Andrii <logotip123@yahoo.com>
2019-12-26 14:59:07 +0200

    add productcategories in models

--- app/models.py
+++ app/models.py
@@ -7,8 +7,13 @@ class OrderArchive(db.Model):
     id_user = db.Column(db.Integer, db.ForeignKey('user.id'))
     id_order = db.Column(db.Integer, db.ForeignKey('orders.id'))
     id_product = db.Column(db.Integer, db.ForeignKey('products.id'))
-    price = db.Column(db.Decimal)
+    price = db.Column(db.Float)
     date_archive = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.id)
+
+
+class ProductCategories(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))

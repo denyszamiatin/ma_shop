8ee61ae1c6129051bce440c2eb883dac268304ca
Senku1 <morhaliuk@gmail.com>
2019-12-26 16:43:13 +0200

    add class Users

--- app/models.py
+++ app/models.py
@@ -53,3 +53,15 @@ class Comments(db.Model):
     id_user = db.Column(db.Integer, db.ForeignKey('user.id'))
     comment_date = db.Column(db.Date, default=datetime.today().date())
     body = db.Column(db.String(255))
+
+
+class Users(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    second_name = db.Column(db.String(50))
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f"<User id: {self.id}>"

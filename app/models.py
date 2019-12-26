2de0eb6d3e92f3aada4bae9fcfe97cd5db51f685
Senku1 <morhaliuk@gmail.com>
2019-12-26 18:02:44 +0200

    class Users update

--- app/models.py
+++ app/models.py
@@ -57,12 +57,30 @@ class Comments(db.Model):
 
 
 class Users(db.Model):
+    """
+    Class to create table "users" in database.
+    Variables
+    ----------
+    id
+    first_name
+    second_name
+    email
+    password
+
+    Methods
+    -------
+    __repr__
+    __str__
+    """
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(50), nullable=False)
-    second_name = db.Column(db.String(50))
-    email = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(255), nullable=False)
+    first_name = db.Column(db.String(500), nullable=False)
+    second_name = db.Column(db.String(500))
+    email = db.Column(db.String(500), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
 
     def __repr__(self):
+        return f"<User id: {self.id}>"
+
+    def __str__(self):
         return f"<User id: {self.id}>"

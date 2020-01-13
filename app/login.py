143c961e9002d70a4248976947e4ddf313a96d6e
DukhDmytro <cowboybebop4991@gmail.com>
2020-01-13 21:48:08 +0200

    task 97. Check user role on admin pages added

--- app/login.py
+++ app/login.py
@@ -1,5 +1,6 @@
 from flask import session, redirect, flash, url_for, request
 from functools import wraps
+from .models import Users
 
 
 def login_required(function):
@@ -12,5 +13,16 @@ def login_required(function):
             session["next_page"] = request.path
             flash("You need to login first")
             return redirect(url_for('login'))
+    return wrap
 
+
+def admin_role_required(function):
+    """Wrap route and check is logined user admin"""
+    @wraps(function)
+    def wrap(*args, **kwargs):
+        user = Users.query.filter_by(id=session["user_id"]).first()
+        if user.admin_role:
+            return function(*args, **kwargs)
+        flash("You don't have admin privileges")
+        return redirect(url_for("index"))
     return wrap
